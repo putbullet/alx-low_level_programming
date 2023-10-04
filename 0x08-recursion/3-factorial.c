1e9c998e82eb54d10351dbdd740b308fbb992b0d
putbullet <soulaimanettabaas@gmail.com>
2023-10-04 12:09:04 -0700

    good factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,16 +9,9 @@
 int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
+	if (n == 0)
 		return (1);
-	}
-	else
-	{
-		return (1 * (factorial(n - 1)));
-	}
+	return (n * factorial(n - 1));
 }
 
