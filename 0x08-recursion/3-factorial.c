77078e88675eeb9ebcddf6745d99a586bd110ff8
putbullet <soulaimanettabaas@gmail.com>
2023-10-04 12:01:11 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,13 +8,13 @@
  */
 int factorial(int n)
 {
-	if (n == 1 || n == 0)
-	{
-		return (1);
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		return (-1);
+	}
+	else if (n = 0)
+	{
+		return (1);
 	else
 	{
 		return (1 * (factorial(n - 1)));
