12f298a4c4d6fde80bd2bc0673292bdca0e68a4f
putbullet <soulaimanettabaas@gmail.com>
2023-10-04 16:39:36 -0700

    prime is real

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,16 +9,16 @@
  */
 int is_prime_recursive(int n, int divisor)
 {
-    if (n <= 1)
-        return (0); /* 0 and 1 are not prime numbers */
+	if (n <= 1)
+		return (0); /* 0 and 1 are not prime numbers */
 
-    if (divisor == 1)
-        return (1); /* If the divisor reaches 1, n is prime */
+	if (divisor == 1)
+		return (1); /* If the divisor reaches 1, n is prime */
 
-    if (n % divisor == 0)
-        return (0); /* n is divisible by divisor, not prime */
+	if (n % divisor == 0)
+		return (0); /* n is divisible by divisor, not prime */
 
-    return (is_prime_recursive(n, divisor - 1)); /* Check with the next divisor */
+	return (is_prime_recursive(n, divisor - 1)); /* Check with the next divisor */
 }
 
 /**
@@ -29,6 +29,6 @@ int is_prime_recursive(int n, int divisor)
  */
 int is_prime_number(int n)
 {
-    return (is_prime_recursive(n, n / 2)); /* Start with the largest possible divisor */
+	return (is_prime_recursive(n, n / 2)); /* Start with the largest possible divisor */
 }
 
